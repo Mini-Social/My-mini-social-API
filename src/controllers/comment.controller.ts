c97aaa96fd7f350fc4a1f4f8b151b7b61aae6ac2
Xuan Duong <nguyenxuanduong1912@gmail.com>
2025-12-23 16:49:17 +0700

    fix: add comment and reply logic v3

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -8,30 +8,43 @@ import { upload } from '@/services/multer'
 import sharp from 'sharp'
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
+import { CleanImages } from '@/utils/CleanImages'
 export const AddComment = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = res.locals.user
-  const { postId } = req.params
-  const { parentCommentId } = req.body
-  const existPost = await PostModel.findById(postId)
-  if (!existPost) {
-    return next(new AppError('No Found Post', 404))
+  const deleteUploadedFile = () => {
+    if (req.file) CleanImages([req.body.image])
   }
-  const existComment = await CommentModel.findById(parentCommentId)
-  if (!existComment) {
-    return next(new AppError('No Found Comment', 404))
+  try {
+    const { id } = res.locals.user
+    const { postId } = req.params
+    const { parentCommentId } = req.body
+    const existPost = await PostModel.findById(postId)
+    if (!existPost) {
+      deleteUploadedFile()
+      return next(new AppError('No Found Post', 404))
+    }
+    if (parentCommentId) {
+      const existComment = await CommentModel.findById(parentCommentId)
+      if (!existComment) {
+        deleteUploadedFile()
+        return next(new AppError('No Found Comment', 404))
+      }
+    }
+    const body = commentSchema.parse(req.body)
+    const data = {
+      postId,
+      userId: id,
+      ...body,
+      parentCommentId: parentCommentId || null
+    }
+    const newComment = await CommentModel.create(data)
+    res.status(200).json({
+      status: 'success',
+      data: { comment: newComment }
+    })
+  } catch (error: any) {
+    CleanImages([req.body.image])
+    return next(new AppError(error.message, 400))
   }
-  const body = commentSchema.parse(req.body)
-  const data = {
-    postId,
-    userId: id,
-    ...body,
-    parentCommentId: parentCommentId || null
-  }
-  const newComment = await CommentModel.create(data)
-  res.status(200).json({
-    status: 'success',
-    data: { comment: newComment }
-  })
 })
 export const UpdateComment = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const { id } = res.locals.user
